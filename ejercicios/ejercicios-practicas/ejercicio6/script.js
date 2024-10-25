fe01058b6d7735eaff5475c007d725c1cca82699
lxstik <egorrfal32@gmail.com>
2024-10-25 19:27:40 +0200

    ej6.1

--- ejercicios/ejercicios-practicas/ejercicio6/script.js
+++ ejercicios/ejercicios-practicas/ejercicio6/script.js
@@ -1,6 +1,12 @@
 //ejercicio 1
-num1 = document.getElementById('num1');
-num2 = document.getElementById('num2');
+document.getElementById('btnEnviar').addEventListener('click', function(){
+    let minimoRandom = parseInt(document.getElementById("num1").value);
+    let maximoRandom = parseInt(document.getElementById("num2").value);
 
-num1 = input.value;
+    aleatorio = Math.floor(Math.random()* (maximoRandom - minimoRandom + 1) + minimoRandom);
+
+    document.getElementById("numeroAleatorio").innerText = "El numero random de " + minimoRandom + " y " + maximoRandom + " es: " + aleatorio;
+}); 
+
+
 
