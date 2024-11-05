ebe06e8868830ff7a43314a2502fb5c93bf7cf11
lxstik <egorrfal32@gmail.com>
2024-11-05 18:59:38 +0100

    lite-server

--- ejercicios/ejercicios-practicas/ejercicio6/script.js
+++ ejercicios/ejercicios-practicas/ejercicio6/script.js
@@ -1,4 +1,4 @@
-//ejercicio 1
+    //ejercicio 1
 document.getElementById('btnEnviar').addEventListener('click', function(){
     let minimoRandom = parseInt(document.getElementById("num1").value);
     let maximoRandom = parseInt(document.getElementById("num2").value);
@@ -8,5 +8,22 @@ document.getElementById('btnEnviar').addEventListener('click', function(){
     document.getElementById("numeroAleatorio").innerText = "El numero random de " + minimoRandom + " y " + maximoRandom + " es: " + aleatorio;
 }); 
 
+    //ejercicio 2
 
+//mayus y minus
+document.getElementById('convertirMayus').addEventListener('click', function() {
+    texto = document.getElementById('texto');
+    texto.value = texto.value.toUpperCase();
+});
 
+document.getElementById('convertirMinus').addEventListener('click', function() {
+    texto = document.getElementById('texto');
+    texto.value = texto.value.toLowerCase(); 
+});
+
+//buscar
+
+document.getElementById('buscarPalab').addEventListener('click', function(){
+    texto2 = document.querySelector('input#buscarInput');
+    texto2 = texto2.trim()
+})
